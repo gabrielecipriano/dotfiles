f4b69a2c60878710be155d4d4326056f40f706d5
Gabriele Cipriano <gabri.cipriano@gmail.com>
2017-11-14 17:54:39 +0100

    pulling dotfiles before install

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ dotfiles_echo() {
 
 set -e # Terminate script if anything exits with a non-zero value
 
+dotfiles_echo "update dotfiles..."
+git pull
+
 DOTFILES_DIR=$HOME/.dotfiles
 VIM_DIR=$DOTFILES_DIR/vim
 
