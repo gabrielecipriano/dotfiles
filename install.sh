8547eb6b7e20b9daa50eb148766c7239084801fa
Gabriele Cipriano <gabri.cipriano@gmail.com>
2017-11-14 16:01:34 +0100

    setting up install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+################################################################################
+# install
+#
+# This script symlinks the dotfiles into place in the home directory.
+################################################################################
+
+dotfiles_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n[DOTFILES] $fmt\n" "$@"
+}
+
+set -e # Terminate script if anything exits with a non-zero value
+
+DOTFILES_DIR=$HOME/dev/dotfiles
+VIM_DIR=$DOTFILES_DIR/vim
+
+files=(
+"gitconfig"
+"gitignore_global"
+"vimrc"
+"zshrc"
+)
+
+dotfiles_echo "Installing dotfiles..."
+
+for file in "${files[@]}"; do
+  if [ -f "$HOME"/."$file" ]; then
+    dotfiles_echo ".$file already present. Backing up..."
+    cp "$HOME"/."$file" "$HOME"/."${file}"_backup
+    rm -f "$HOME"/."$file"
+  fi
+  dotfiles_echo "-> Linking $DOTFILES_DIR/$file to $HOME/.$file..."
+  ln -nfs "$DOTFILES_DIR"/"$file" "$HOME"/."$file"
+done
+
+dotfiles_echo "-> Linking $DOTFILES_DIR/Brewfile to $HOME/Brewfile..."
+ln -nfs "$DOTFILES_DIR"/Brewfile "$HOME"/Brewfile
+
+if [ -d "$HOME"/.vim/ftplugin ]; then
+  rm -rf "$HOME"/.vim/ftplugin
+fi
+ln -nfs "$VIM_DIR"/ftplugin "$HOME"/.vim/ftplugin
+
+dotfiles_echo "Dotfiles installation complete!"
+dotfiles_echo "Complete Brew Bundle installation with 'brew bundle install -v --global'"
