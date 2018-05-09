b63eaa668b1ea9f57deab5d6a173d0aed5ba4374
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-05-09 22:41:15 +0100

    improved font installing part

--- setup-macos.sh
+++ setup-macos.sh
@@ -4,9 +4,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
+fonts/install.sh
 rm -rf fonts
 
 brew install \
