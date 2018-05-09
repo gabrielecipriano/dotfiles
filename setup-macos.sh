e4ff610e7383e85c3edeb314c1f69b33de62e2a5
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-05-09 22:03:33 +0100

    added setup mac osx script tool

---
+++ setup-macos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+brew install \
+  the_silver_searcher \
+  wget \
+  watch \
+  python \
+  python3 \
+  ruby \
+  elixir \
+  haskell-stack \
+  git 
+
+brew cask install docker
+brew cask install iterm2
+brew cask install spotify
+brew cask install intellij-idea-ce
+brew cask install slack
+brew cask install caskroom/versions/java8
+
+brew cleanup
+brew prune
