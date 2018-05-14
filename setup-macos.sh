4f188e7b5c6ee784a95fbc45253a483ec97d26d1
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-05-14 11:04:44 +0100

    vim config installation was missing

--- setup-macos.sh
+++ setup-macos.sh
@@ -3,6 +3,11 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+#VIM-CONFIG
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+#POWERLINE-FONTS
 git clone https://github.com/powerline/fonts.git --depth=1
 fonts/install.sh
 rm -rf fonts
@@ -16,7 +21,7 @@ brew install \
   ruby \
   elixir \
   haskell-stack \
-  git 
+  git
 
 brew cask install docker
 brew cask install iterm2
