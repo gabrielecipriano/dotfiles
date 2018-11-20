0c8f5343d629efeeaa65761c9314670642a88f3a
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-11-20 10:36:35 +0000

    improving mac setup

--- systems/macosx/setup.sh
+++ systems/macosx/setup.sh
@@ -3,6 +3,8 @@
 #Installing brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+xcode-select --install
+
 curl https://sh.rustup.rs -sSf | sh
 
 brew unlink python
@@ -28,6 +30,7 @@ brew install \
   haskell-stack \
   cowsay \
   httpie \
+  bash \
   awscli \
   bat \
   lolcat
@@ -46,3 +49,6 @@ brew cask install virtualbox-extension-pack
 
 brew cleanup
 brew prune
+
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
--- zshrc
+++ zshrc
@@ -125,5 +125,5 @@ DEFAULT_USER='gcipriano'
 source $HOME/.zprofile
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/home/gcipriano/.sdkman"
-[[ -s "/home/gcipriano/.sdkman/bin/sdkman-init.sh" ]] && source "/home/gcipriano/.sdkman/bin/sdkman-init.sh"
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
