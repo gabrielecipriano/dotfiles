7dab94749e5a0ee40d9e83483610f9f13d3386f3
Gabriele Cipriano <gabrielecipriano@gcipriano-a02.vmware.com>
2021-10-12 12:11:56 +0200

    small fixes

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ install_ansible() {
     sudo apt install ansible -y
   elif [[ "$platform" == "macos" ]]; then
     log "Installing/upgrading Homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew install ansible
   fi
 }
